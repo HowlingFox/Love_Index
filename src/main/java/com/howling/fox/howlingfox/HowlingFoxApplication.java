5ae2de71f92301e11f999e1d6cc03a8f7b6a5ce6
lujie <3418427916@qq.com>
2022-02-18 22:37:44 +0800

    初始化项目

---
+++ src/main/java/com/howling/fox/howlingfox/HowlingFoxApplication.java
@@ -0,0 +1,13 @@
+package com.howling.fox.howlingfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowlingFoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowlingFoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/howling/fox/howlingfox/HowlingFoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howling.fox.howlingfox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowlingFoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
